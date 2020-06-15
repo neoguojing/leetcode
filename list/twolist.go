9942e1452b6b8404cf1e8a4716025e4a8f5169ad
neoguojing <guojing_neo@163.com>
2020-06-16 02:49:05 +0800

    fix sum issue

--- list/twolist.go
+++ list/twolist.go
@@ -1,20 +1,20 @@
 package list
 
-import(
+import (
 	"fmt"
 )
 
 type ListNode struct {
 	Next *ListNode
-	Val int
+	Val  int
 }
 
-func GeneListByArray(in []int) *ListNode{
+func GeneListByArray(in []int) *ListNode {
 	head := &ListNode{}
 	cur := head
-	for _,v := range in {
+	for _, v := range in {
 		cur.Next = &ListNode{
-			Val:v,
+			Val: v,
 		}
 		cur = cur.Next
 	}
@@ -26,13 +26,12 @@ func Print(head *ListNode) {
 	cur := head
 	out := ""
 	for cur != nil {
-		out += fmt.Sprintf("%d->",cur.Val)
+		out += fmt.Sprintf("%d->", cur.Val)
 		cur = cur.Next
 	}
 
 	fmt.Println(out)
 }
-
 
 //no 2
 func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
@@ -41,13 +40,14 @@ func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	carry := 0
 	sum := 0
 	cur := 0
-    for;l1 != nil || l2 != nil; {
-		if l1 == nil {
+	for l1 != nil || l2 != nil {
+
+		if l1 == nil && l2 != nil {
 			sum = l2.Val + carry
 			l2 = l2.Next
 		}
 
-		if l2 == nil {
+		if l2 == nil && l1 != nil {
 			sum = l1.Val + carry
 			l1 = l1.Next
 		}
@@ -57,14 +57,14 @@ func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			l1 = l1.Next
 			l2 = l2.Next
 		}
-		
+
 		cur = sum % 10
 		carry = sum / 10
 
 		pos.Next = &ListNode{}
 		pos.Next.Val = cur
 		pos = pos.Next
-		
+
 	}
 
 	if carry != 0 {
@@ -75,12 +75,11 @@ func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	return head.Next
 }
 
-
 func AddTwoNumbersRecur(l1 *ListNode, l2 *ListNode) *ListNode {
 	return nil
 }
 
-func addTwoNumbersRecur(l1 *ListNode, l2 *ListNode,carry int,output *ListNode){
+func addTwoNumbersRecur(l1 *ListNode, l2 *ListNode, carry int, output *ListNode) {
 	if l1 == nil {
 
 	}
