b0a03baa24eac7f58e587b88dca699a5b2dff967
neoguojing <guojing_neo@163.com>
2020-06-16 17:35:49 +0800

    up

--- array/two_array.go
+++ array/two_array.go
@@ -1,9 +1,20 @@
 package array
 
-import()
+import(
+	"sort"
+)
 
 //已知两个有序数组，找到两个数组合并后的中位数
 //
-func FindMedianSortedArrays() int{
-	return 0
+func FindMedianSortedArrays(nums1,nums2 []int) float32{
+
+	nums1 = append(nums1,nums2...)
+	sort.Ints(nums1)
+
+	mid := len(nums1)/2
+	if len(nums1)%2 == 0 {
+		return float32((nums1[mid]+nums1[mid-1]))/2.0
+	} 
+
+	return float32(nums1[mid])
 }
