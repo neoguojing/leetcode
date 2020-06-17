7e18a86d053adb27060ed448ea173519d7ab355f
neoguojing <guojing_neo@163.com>
2020-06-18 02:38:20 +0800

    up

--- string/regular.go
+++ string/regular.go
@@ -1,30 +1,29 @@
 package string
 
-import()
-
-//「点.」代表任意字符，「星号*」 代表前一个字符重复 0 次或任意次
-//dp[i][j] 代表以i开头到结尾的字符串和以j开头的字符串是否匹配
-//. ab  a.    dp[i][j] = dp[i+1][j+1]
-//* aa a*  ab a*b   dp[i][j] = dp[i+1][j+1]&&t[i-1]==t[i]||dp[i+1][j+1]&&t[i]==p[j+1]
-func IsMatch(text,patten string) bool {
+/*IsMatch
+1, If p[j] == s[i] :  dp[i][j] = dp[i-1][j-1];
+2, If p[j] == '.' : dp[i][j] = dp[i-1][j-1];
+3, If p[j] == '*':
+               1   if p[j-1] != s[i] : dp[i][j] = dp[i][j-2]  //aab* aa  b*可以代表空字符串
+               2   if p[j-1] == s[i] or p[j-1]== '.':
+                              dp[i][j] = dp[i-1][j]    // ab* abbb
+                           or dp[i][j] = dp[i][j-1]   // abb*   abb
+                           or dp[i][j] = dp[i][j-2]   // abb*  ab
+*/
+func IsMatch(text, patten string) bool {
 	m := len(text)
 	n := len(patten)
-	dp := initDP2Ds(m+1,n+1)
-	dp[m][n] = true
-	for i:=m-1;i>=0;i--{
-		for j:=n-1;j>=0;j--{
-			if text[i] == patten[j] && dp[i+1][j+1] {
-				dp[i][j] = true
-			} else if patten[j]==byte('.') && dp[i+1][j+1]{
-				dp[i][j] = true
-			} else{
-				if (i > 0) {
-					if patten[j]==byte('*') && dp[i+1][j+1] && (text[i]==text[i-1]||text[i]==patten[j+1]) {
-						dp[i][j] = true
-					}
-				} 
-			} 
+	dp := initDP2Ds(m+1, n+1)
+	dp[0][0] = true
+	for i := 0; i < n; i++ {
+		if patten[i] == '*' && dp[0][i-1] {
+			dp[0][i+1] = true
 		}
 	}
-	return dp[0][0]
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+
+		}
+	}
+	return dp[m][n]
 }
