98af699e36984b4b614bf38e4293a9f0f6b434e9
neoguojing <guojing_neo@163.com>
2020-06-19 01:03:41 +0800

    up

---
+++ string/parentheses.go
@@ -0,0 +1,55 @@
+package string
+
+import (
+	"leetcode/utils"
+)
+
+/*
+20 括号匹配问题
+*/
+func IsParenthesesValid(in string) bool {
+	s := utils.NewStack()
+
+	for _, v := range in {
+		switch v {
+		case '(', '[', '{':
+			s.Push(v)
+		case ')':
+			if s.Empty() {
+				return false
+			}
+			if s.Peak().(byte) == '(' {
+				s.Pop()
+			} else {
+				return false
+			}
+		case '}':
+			if s.Empty() {
+				return false
+			}
+			if s.Peak().(byte) == '{' {
+				s.Pop()
+			} else {
+				return false
+			}
+
+		case ']':
+			if s.Empty() {
+				return false
+			}
+			if s.Peak().(byte) == '[' {
+				s.Pop()
+			} else {
+				return false
+			}
+		default:
+			continue
+		}
+	}
+
+	if s.Empty() {
+		return true
+	}
+
+	return false
+}
