e485eefec3ab867e54e0bafd88c4c51ade619332
neoguojing <guojing_neo@163.com>
2020-06-20 14:29:01 +0800

    add queue util

--- combination/index.go
+++ combination/index.go
@@ -4,4 +4,13 @@ package combination
 排列组合
 
 17 给一串数字，每个数可以代表数字键下的几个字母，返回这些数字下的字母的所有组成可能
+1.相乘
+2.队列
+3.迭代
 */
+
+digitLetter := []string{ "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" }
+func LetterCombinations(digits string) []string{
+	
+
+}
