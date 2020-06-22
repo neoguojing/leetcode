d4dbc7544f21415144c9cc3e827368b444215142
neoguojing <guojing_neo@163.com>
2020-06-22 21:22:11 +0800

    add min edit distance

---
+++ utils/compare.go
@@ -0,0 +1,41 @@
+package utils
+
+//Max ...
+func Max(in ...int) int {
+	if in == nil {
+		return 0
+	}
+
+	if len(in) == 0 {
+		return in[0]
+	}
+
+	max := in[0]
+	for _, v := range in {
+		if v > max {
+			max = v
+		}
+	}
+
+	return max
+}
+
+//Min ...
+func Min(in ...int) int {
+	if in == nil {
+		return 0
+	}
+
+	if len(in) == 0 {
+		return in[0]
+	}
+
+	min := in[0]
+	for _, v := range in {
+		if v < min {
+			min = v
+		}
+	}
+
+	return min
+}
