94d07abead5a3ac2912b0160cef6dd333c604ef7
neoguojing <guojing_neo@163.com>
2020-07-21 21:34:56 +0800

    add stock

---
+++ array/stock.go
@@ -0,0 +1,29 @@
+package array
+
+// MaxProfit1 ...
+// no 121
+//给一个数组，看作每天股票的价格，然后某一天买入，某一天卖出，最大收益可以是多少。可以不操作，收入就是 0。
+func MaxProfit1(prices []int) int {
+	return 0
+}
+
+// MaxProfit2 ...
+// no 122
+// 可以不停的买入、卖出，但是只有卖出了才能继续买入
+func MaxProfit2(prices []int) int {
+	return 0
+}
+
+// MaxProfit3 ...
+// no 123
+// 给一个数组代表股票每天的价格。你最多可以买入卖出两次，但只有卖出了才可以再次买入，求出最大的收益是多少
+func MaxProfit3(prices []int) int {
+	return 0
+}
+
+// MaxProfit4 ...
+// no 188
+//给一个数组代表股票每天的价格。你最多可以买入卖出 K 次，但只有卖出了才可以再次买入，求出最大的收益是多少
+func MaxProfit4(prices []int) int {
+	return 0
+}
