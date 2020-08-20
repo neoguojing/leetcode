342e6dcf93c617b663413b9f8bf3ff2a9d524f36
neoguojing <guojing_neo@163.com>
2020-08-20 14:32:27 +0800

    up

--- graph/index.go
+++ graph/index.go
@@ -1,1 +1,22 @@
 package graph
+
+// Graph 邻接表
+// 适合稀疏图
+type Graph struct {
+	E_num int
+	V_num int
+	Adj   []Vertex
+}
+
+// Vertex 节点
+type Vertex struct {
+	Val       int
+	FirstEdge *Edge
+}
+
+// Edge 边
+type Edge struct {
+	// 节点数组中的索引位置
+	Idx  int
+	Next *Edge
+}
