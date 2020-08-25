1f12420840876917d237800f21c4c9189bb26a95
neoguojing <guojing_neo@163.com>
2020-08-25 17:44:49 +0800

    Add palindromic

--- string/palindromic.go
+++ string/palindromic.go
@@ -1,9 +1,14 @@
 package string
 
+import (
+	"leetcode/utils"
+)
+
 //LongestPalindrome ...
+// no 5
 //给定一个字符串，输出最长的回文子串。回文串指的是正的读和反的读是一样的字符串
 func LongestPalindrome(in string) string {
-	return longestPalindromeByDP1(in)
+	return longestPalindromeByDP(in)
 }
 
 //1.双重循环
@@ -47,28 +52,83 @@ func longestPalindromeByDP(in string) string {
 	return ret
 }
 
-// todo
-func longestPalindromeByDP1(in string) string {
-	n := len(in)
-	dp := make([]bool, n)
-	for i := range dp {
-		dp[i] = false
+// LongestPalindromeSubseq ...
+// no 516 求最长回文子序列 而不是子串
+func LongestPalindromeSubseq(in string) int {
+	if len(in) == 0 {
+		return 0
 	}
-	rs := []rune(in)
-	maxLen := 0
-	ret := ""
-	for i := n - 1; i >= 0; i-- {
-		for j := n - 1; j >= i; j-- {
-			if in[i] == in[j] && (j-i < 3 || dp[j-1]) {
-				dp[j] = true
-			}
+	n := len(in)
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, 0)
+		for j := 0; j < n; j++ {
+			if i == j {
+				dp[i] = append(dp[i], 1)
 
-			if dp[j] && maxLen < j-i+1 {
-				maxLen = j - i + 1
-				ret = string(rs[i : j+1])
+			} else {
+				dp[i] = append(dp[i], 0)
 			}
 		}
 	}
 
-	return ret
+	maxLen := 0
+	for i := n - 1; i >= 0; i-- {
+		for j := i + 1; j < n; j++ {
+			if in[i] == in[j] {
+				if j-i == 1 {
+					dp[i][j] = 2
+				} else {
+					dp[i][j] = dp[i+1][j-1] + 2
+				}
+			} else {
+				dp[i][j] = utils.Max(dp[i+1][j], dp[i][j-1])
+			}
+
+			if maxLen < dp[i][j] {
+				maxLen = dp[i][j]
+			}
+		}
+	}
+
+	return maxLen
+}
+
+//CountSubstrings ...
+// no 647
+// 给定一个字符串，你的任务是计算这个字符串中有多少个回文子串。
+// 具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
+// dp[i][j] ==
+func CountSubstrings(in string) int {
+
+	if len(in) == 0 {
+		return 0
+	}
+
+	if len(in) == 1 {
+		return 1
+	}
+
+	n := len(in)
+	dp := make([][]bool, n)
+	for i := range dp {
+		dp[i] = make([]bool, 0)
+		for j := 0; j < n; j++ {
+			dp[i] = append(dp[i], false)
+		}
+	}
+	count := 0
+	for i := n - 1; i >= 0; i-- {
+		for j := i; j < n; j++ {
+			if in[i] == in[j] && i-j+1 <= 2 {
+				count++
+				dp[i][j] = true
+			} else if in[i] == in[j] && dp[i+1][j-1] {
+				dp[i][j] = true
+				count++
+			}
+		}
+	}
+
+	return count
 }
---
+++ string/test/palindromic_test.go
@@ -0,0 +1,32 @@
+package test
+
+import (
+	strings "leetcode/string"
+	"testing"
+)
+
+func TestLongestPalindrome(t *testing.T) {
+	in := "babad"
+
+	t.Log(strings.LongestPalindrome(in))
+	in = "cbbd"
+	t.Log(strings.LongestPalindrome(in))
+}
+
+func TestCountSubstrings(t *testing.T) {
+	in := "abc"
+
+	t.Log(strings.CountSubstrings(in))
+	in = "aaa"
+	t.Log(strings.CountSubstrings(in))
+
+}
+
+func TestLongestPalindromeSubseq(t *testing.T) {
+	in := "bbbab"
+
+	t.Log(strings.LongestPalindromeSubseq(in))
+	in = "cbbd"
+	t.Log(strings.LongestPalindromeSubseq(in))
+
+}
