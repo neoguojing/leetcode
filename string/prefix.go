15b50aaf7f2fd2b7dca4edc11496038c51d0dfb6
guoerjun <guoerjun@sensetime.com>
2021-11-04 14:19:07 +0800

    add doc

--- string/prefix.go
+++ string/prefix.go
@@ -4,9 +4,16 @@ package string
 // no 14
 // 最长公共前缀
 // 递归法
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// 1.公共前缀和长度有关，只需要比较长度最短的字符串的个数
 func LongestCommonPrefix(strs []string) string {
-	if strs == nil || len(strs) == 0 || len(strs) == 1 {
+	if strs == nil || len(strs) == 0 {
 		return ""
+	}
+
+	if len(strs) == 1 {
+		return strs[0]
 	}
 
 	min := 1000000
@@ -26,6 +33,7 @@ func LongestCommonPrefix(strs []string) string {
 		for j := 1; j < len(strs); j++ {
 			if strs[j-1][i] != strs[j][i] {
 				flag = false
+				break
 			}
 		}
 		if flag {
--- string/string.go
+++ string/string.go
@@ -55,6 +55,36 @@ func isInterleave(s1, s2, s3 string, i, j, k int) bool {
 	return false
 }
 
+// ReverseStr ...
+// no 541
+// Input: s = "abcdefg", k = 2
+// Output: "bacdfeg"
+// 开头k个字符反转，同时每隔2k个字符反转依次，每次反转k个字符
+// 等价于：每2k字符，反转前k个
+func ReverseStr(s string, k int) string {
+	if k > len(s) {
+		return reverseStr(s)
+	}
+
+	if len(s) > k && len(s) < 2*k {
+		return reverseStr(s[:k]) + s[k:]
+	}
+	var ret string
+	ret = reverseStr(s[:k]) + s[k:]
+	ret += ReverseStr(s[2*k:], k)
+
+	return ret
+}
+
+// reverseStr ...
+func reverseStr(s string) string {
+	var ret string
+	for i := 0; i < len(s); i++ {
+		ret += string(s[len(s)-1-i])
+	}
+	return ret
+}
+
 // no 76
 // 给两个字符串，S 和 T，在 S 中找出包含 T 中所有字母的最短字符串，不考虑顺序。
 // todo
