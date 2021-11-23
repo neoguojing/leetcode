fd0074c43b014be1e858c97460551396cfd16e15
guoerjun <guoerjun@sensetime.com>
2021-11-23 21:14:16 +0800

    up

--- array/two_array.go
+++ array/two_array.go
@@ -20,3 +20,33 @@ func FindMedianSortedArrays(nums1, nums2 []int) float32 {
 
 	return float32(nums1[mid])
 }
+
+// Merge 将两个排序的数组合并,排序结果存储在nums1中
+// no 88
+func Merge(nums1 []int, m int, nums2 []int, n int) {
+	j := n - 1
+	k := m + n - 1
+	i := m - 1
+	for ; k >= 0 && i >= 0 && j >= 0; k-- {
+		if nums1[i] > nums2[j] {
+			nums1[k] = nums1[i]
+			i--
+		} else {
+			nums1[k] = nums2[j]
+			j--
+		}
+	}
+
+	for ; i >= 0; i-- {
+		nums1[k] = nums1[i]
+		k--
+	}
+
+	for ; j >= 0; j-- {
+		nums1[k] = nums2[j]
+		k--
+	}
+
+}
+
+// 977
