400609af22591b3a0c5b193fe9deaf6dee14197b
guoerjun <guoerjun@sensetime.com>
2021-12-03 12:16:10 +0800

    up

--- array/jump.go
+++ array/jump.go
@@ -3,9 +3,9 @@ package array
 // CanJump ...
 // no 55
 //从数组的第 0 个位置开始跳，跳的距离小于等于数组上对应的数.判断是否能跳到最后一个位置
-//
+//  [2,3,1,1,4]
 func CanJump(nums []int) bool {
-	if nums == nil || len(nums) == 0 {
+	if len(nums) == 0 {
 		return true
 	}
 
@@ -29,6 +29,40 @@ func CanJump(nums []int) bool {
 	return true
 }
 
+// 贪心算法
+func CanJump2(nums []int) bool {
+	far := 0
+
+	for i := 0; i < len(nums); i++ {
+		if far < i {
+			return false
+		}
+		if far < nums[i]+i {
+			far = nums[i] + i
+		}
+
+	}
+
+	return true
+}
+
+// 动态规划
+func CanJump3(nums []int) bool {
+	dp := make([]bool, len(nums))
+	dp[len(nums)-1] = true
+	for i := len(nums) - 2; i >= 0; i-- {
+		for step := 0; step <= nums[i] && (i+step) < len(nums); step++ {
+			if dp[i+step] {
+				dp[i] = true
+				goto HEAR
+			}
+		}
+	HEAR:
+	}
+
+	return dp[0]
+}
+
 // Jump ...
 // no 45
 // 从数组的第 0 个位置开始跳，跳的距离小于等于数组上对应的数。求出跳到最后个位置需要的最短步数
@@ -36,7 +70,7 @@ func CanJump(nums []int) bool {
 //1 贪心算法
 // 2.从末尾开始，每次找能跳到当前位置的最远位置,直到到达0的位置
 func Jump(nums []int) int {
-	if nums == nil || len(nums) == 0 {
+	if len(nums) == 0 {
 		return 0
 	}
 
@@ -44,7 +78,7 @@ func Jump(nums []int) int {
 	steps := 0
 	for positon != 0 {
 		for i := 0; i < positon; i++ {
-			if nums[i] >= (positon - i) {
+			if nums[i] >= (positon - i) { //i 从0开始，保证每次找到了跳的最远的位置
 				positon = i
 				steps++
 				break
@@ -54,3 +88,39 @@ func Jump(nums []int) int {
 
 	return steps
 }
+
+// 贪心策略
+func Jump2(nums []int) int {
+	least := int(^uint32(0) >> 1)
+
+	for i := 0; i < len(nums); i++ {
+
+	}
+
+	return least
+}
+
+// 动态规划
+// dp[n-1] = max
+// dp[i] = min(dp[i+k] +1)
+func Jump3(nums []int) int {
+	dp := make([]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		dp[i] = len(nums)
+	}
+	dp[len(nums)-1] = 0
+	for i := len(nums) - 2; i >= 0; i-- {
+		min := len(nums)
+		for step := 0; step <= nums[i] && (i+step) < len(nums); step++ {
+			if dp[i+step]+1 < min {
+				min = dp[i+step] + 1
+			}
+		}
+		dp[i] = min
+	}
+
+	return dp[0]
+}
+
+// no 1036
+// no 1871
