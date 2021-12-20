6bbd6f2251d64de124c40331ac690f93fbe778e1
guoerjun <guoerjun@sensetime.com>
2021-12-20 19:53:10 +0800

    up

--- stack/index.go
+++ stack/index.go
@@ -1,5 +1,10 @@
 package stack
 
+import (
+	"container/list"
+	"math"
+)
+
 type Stack []int
 
 func (s *Stack) Push(a int) {
@@ -21,4 +26,43 @@ func (s *Stack) Top() int {
 		return (*s)[len(*s)-1]
 	}
 	return -1
+}
+
+type MinStack struct {
+	min  int
+	list *list.List
+}
+
+func Constructor() MinStack {
+	return MinStack{
+		list: list.New(),
+		min:  math.MaxInt64,
+	}
+}
+
+func (this *MinStack) Push(val int) {
+	if val < this.min {
+		this.min = val
+	}
+	this.list.PushBack(val)
+}
+
+func (this *MinStack) Pop() {
+	e := this.list.Back()
+	if e != nil {
+		this.list.Remove(e)
+	}
+}
+
+func (this *MinStack) Top() int {
+	e := this.list.Back()
+	if e != nil {
+		return -1
+	}
+
+	return e.Value.(int)
+}
+
+func (this *MinStack) GetMin() int {
+	return this.min
 }
