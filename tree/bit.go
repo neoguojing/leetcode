241af1c8bad03531de383cdec607c720e3e785a9
guoerjun <guoerjun@sensetime.com>
2022-01-10 11:05:53 +0800

    up

--- tree/bit.go
+++ tree/bit.go
@@ -23,7 +23,7 @@ func NewBiTree(nums []int) BITree {
 func (bit BITree) Add(i int, val int) {
 	i = i + 1
 	for ; i < len(bit); i = getNext(i) {
-		bit[i] = val
+		bit[i] += val
 	}
 }
 
@@ -39,18 +39,41 @@ func (bit BITree) GetSum(i int) int {
 	return sum
 }
 
-//RangeSum  i->j的和
+//RangeSum  i+1->j的和
 func (bit BITree) RangeSum(i, j int) int {
 	if i > j {
 		return -1
 	}
-
-	i = i + 1
 	sum := 0
-	for i > 0 {
-		sum += bit[i]
-		i -= getNext(i)
+	i = i + 1
+	j = j + 1
+
+	for ; j > i; j = getParent(j) {
+		sum += bit[j]
+	}
+	for ; i > j; i = getParent(i) {
+		sum -= bit[i]
 	}
 
 	return sum
+}
+
+func (bit BITree) Get(i int) int {
+	return bit.RangeSum(i-1, i)
+}
+
+func (bit BITree) Set(i, val int) {
+	bit.Add(i, val-bit.Get(i))
+}
+
+func (bit BITree) GetArray() []int {
+	ret := make([]int, len(bit)-1)
+	for i := len(bit) - 1; i > 0; i-- {
+		j := getNext(i)
+		if j < len(bit) {
+			ret[j-1] = bit[j] - bit[i]
+		}
+
+	}
+	return ret
 }
--- tree/test/bit_test.go
+++ tree/test/bit_test.go
@@ -7,6 +7,13 @@ import (
 
 func TestBiTree(t *testing.T) {
 	bit := tree.NewBiTree([]int{1, 2, 3, 4, 5})
-	t.Log(bit.GetSum(0))
-	t.Log(bit)
+
+	t.Log(bit.GetSum(3))
+	t.Log(bit.RangeSum(2, 4))
+	t.Log(bit.Get(4))
+	t.Log(bit.GetArray())
+	bit.Set(4, 6)
+	t.Log(bit.RangeSum(2, 4))
+	t.Log(bit.Get(4))
+	t.Log(bit.GetArray())
 }
